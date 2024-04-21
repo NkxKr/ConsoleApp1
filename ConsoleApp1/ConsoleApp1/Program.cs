ad3b3ba5ff645eecb847ebbecf3934ab1289057f
Nikodem Krawiec <nkkr080703@gmail.com>
2024-04-21 18:50:00 +0200

    Zadanie domowe dzień 4 v3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Cześć Nikodem");
+
+string name = "Nikodem";
+string gender = "men";
+int age = 21;
+
+if (gender == "women")
+{
+    if (age ==30)
+    {
+        if (name == "Ewa")
+        {
+            Console.WriteLine("Ewa lat 30");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Niedotyczy");
+}
