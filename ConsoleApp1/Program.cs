517abc2a8dfd8eb8a742c7a1bd430a661364d24e
Nikodem Krawiec <nkkr080703@gmail.com>
2024-04-21 16:29:49 +0200

    Zadanie domowe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,21 @@
 ﻿Console.WriteLine("Cześć Nikodem");
+
+string name = "Nikodem";
+int age = 17;
+string gender = "Men";
+
+if (gender == "Women")
+{
+    if (age == 30)
+    {
+        if (name == "Ewa")
+        {
+            Console.WriteLine("Ewa lat 30");
+        }
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
