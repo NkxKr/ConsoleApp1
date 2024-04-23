b8417d237fafba278d3da00e7e9f67d13d41b273
Nikodem Krawiec <nkkr080703@gmail.com>
2024-04-23 21:52:56 +0200

    Zadanie domowe dzień 4 v8

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,19 +1,18 @@
 ﻿Console.WriteLine("Cześć Nikodem");
 
-
 string name = "Nikodem";
 string gender = "men";
-var age = 21;
+int age = 21;
 
-if (gender == "women" && age ==30)
+if (gender == "women" && age < 30)
 {
-        Console.WriteLine("Kobieta poniżej lat 30");
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
 else if (name == "Ewa" && age == 30)
-{   
-        Console.WriteLine("Ewa lat 30");
+{
+    Console.WriteLine("Ewa lat 30");
 }
 else if (gender == "men" && age < 18)
 {
-        Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine("Niepełnoletni mężczyna");
 }
