f1ab517d23e72e9c1505e99a962b4ce8183580d7
Nikodem Krawiec <nkkr080703@gmail.com>
2024-04-23 23:44:34 +0200

    Zadanie domowe dzień 6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,57 @@
-﻿Console.WriteLine("Cześć Nikodem");
+﻿using ConsoleApp1;
 
-string name = "Nikodem";
-string gender = "men";
-int age = 21;
+Console.WriteLine("Cześć Nikodem");
 
-if (gender == "women" && age < 30)
+
+Employee employee1 = new Employee("Nikodem", "Krawiec", 21);
+Employee employee2 = new Employee("Przemek", "Mazur", 33);
+Employee employee3 = new Employee("Karolina", "Mazur", 30);
+Employee employee4 = new Employee("Roman", "Dąb", 19);
+
+employee1.AddScore(10);
+employee1.AddScore(9);
+employee1.AddScore(8);
+employee1.AddScore(7);
+employee1.AddScore(6);
+
+employee2.AddScore(0);
+employee2.AddScore(1);
+employee2.AddScore(2);
+employee2.AddScore(3);
+employee2.AddScore(4);
+
+employee3.AddScore(0);
+employee3.AddScore(2);
+employee3.AddScore(4);
+employee3.AddScore(6);
+employee3.AddScore(8);
+
+employee4.AddScore(1);
+employee4.AddScore(3);
+employee4.AddScore(5);
+employee4.AddScore(7);
+employee4.AddScore(9);
+
+List<Employee> employees = new List<Employee>()
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 30)
+    employee1, employee2, employee3, employee4,
+};
+
+
+var maxResult = 0;
+Employee EmployeeWithMaxResult = null;
+
+foreach (var employee in employees)
 {
-    Console.WriteLine("Ewa lat 30");
+    if (employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        EmployeeWithMaxResult = employee;
+    }
 }
-else if (gender == "men" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyna");
-}
+
+Console.WriteLine("Najlepszy pracownik");
+Console.WriteLine("Imię:" + EmployeeWithMaxResult.Name);
+Console.WriteLine("Nazwisko:" + EmployeeWithMaxResult.Surname);
+Console.WriteLine("Wiek:" + EmployeeWithMaxResult.Age);
+Console.WriteLine("Wynik" + EmployeeWithMaxResult.Result);
